319fba4e58c931420843aefe6d8e628f07adbf5d
pmandrik <petr.mandrik@cern.ch>
2023-06-04 01:26:25 +0300

    update Dialogs

--- ShtaHLT/ShtaDialogs.hh
+++ ShtaHLT/ShtaDialogs.hh
@@ -7,124 +7,185 @@
 #include <vector>
 #include <string>
 
+#include "pmlib_other.hh"
+#include "pmlib_msg.hh"
+#include "pmlib_v2d.hh"
+
 namespace ge {
 
   using namespace std;
 
   // @@@Decoupled from ShtaEngine@@@
   // Dialog() machine
-  //  - in general we are working with abstract "dialog entry" = <DialogEntry id="" data=... next_id="" mark=""> and <DialogAnswer id="" data="" next_id="" mark=""/>
+  //  - in general we are working with abstract "dialog Speech" = <DialogSpeech id="" data=... next_id="" mark=""> and <DialogAnswer id="" data="" next_id="" mark=""/>
   //  e.g dialog data is string, dialog answers and vector<string>
   //  - D GetData() -> return dialog data
   //  - vector<A> GetAnswers() -> return dialog answers data
-  //  - PutAnswer(int id) -> give answer to dialog, switching dialog to another "dialog entry", switching state to "new"
-  //  - bool MoveTo(string id) -> move to dialog entry with id
+  //  - PutAnswer(int id) -> give answer to dialog, switching dialog to another "dialog Speech", switching state to "new"
+  //  - bool MoveTo(string id) -> move to dialog Speech with id
   //  - GetState() -> 
-  //      "none" when no "dialog entry" available
+  //      "none" when no "dialog Speech" available
   //      "ready" - before first call Start()
-  //      "active" - has dialog entry available
+  //      "active" - has dialog Speech available
   //      "end" - no more entries available in the dialog tree
-  //  - vector <string> GetRecord() -> return record of all events as vector< pair<string,string> > of "dialog entry" ids, answer ids 
+  //  - vector <string> GetRecord() -> return record of all events as vector< pair<string,string> > of "dialog Speech" ids, answer ids 
   //  - GetMarks() -> return record of all events as vector<string> of "dialog answers" marks, where mark is from answer
   //  - Start(string id) -> Reset(), 
   //  - Reset() -> clear recors and marks, MoveTo() id used with Start call
   
   // DialogFactory() TODO
-  //  - return Dialog() class filled with DialogEntry() items from input format
+  //  - return Dialog() class filled with DialogSpeech() items from input format
   //  - e.g. from XMLs:
-  //      <DialogEntry id="" data="" next_id="" mark="">
-  //        <DialogAnswer id="" data="" next_id="" mark=""/>
-  //      </DialogEntry>
-  //    in general, DialogEntry.data and DialogAnswer.data may have different ay types D & A
+  //      <Dialog id="">
+  //       <DialogSpeech id="" data="" next_id="" mark="">
+  //         <DialogAnswer id="" data="" next_id="" mark=""/>
+  //         <DialogAnswer id="" data="" next_id="" mark=""/>
+  //         <DialogAnswer id="" data="" next_id="" mark=""/>
+  //       </DialogSpeech>
+  //       <DialogSpeech id="" data="" next_id="" mark="">
+  //         <DialogAnswer id="" data="" next_id="" mark=""/>
+  //         <DialogAnswer id="" data="" next_id="" mark=""/>
+  //         <DialogAnswer id="" data="" next_id="" mark=""/>
+  //       </DialogSpeech>
+  //      </Dialog>
+  //    in general, DialogSpeech.data and DialogAnswer.data may have different ay types D & A
   
-  // DialogProxy(): TODO
-  //  - tick_per_letter - speed of revealed letters
-  //  - dialog box width W, letter W
-  //  - void Tick( action )
-  //    - if "Z" action and not ready -> skip to ready and let GetData() return whole answer
-  //    - if no actions and not read -> update timer and maybe let GetData() return more symbols
-  //    - if "Z" action and ready -> 
-  //    - we have some delay between "Z" actons are acceptable 
-  //    - if arrows actions -> change between answers marked to be next
-  //    - return Flase when dialog "end"
-  //  - string GetData() - get formatted data (entry) suitable for the dialog box 
-  //  - string GetAnswers() - get formatted data (entry) suitable for the dialog box 
+  // DialogProxy(): simple class to 
+  //  1. giving the size of the data calculate what is amount of data to show at this point of time if timing per data unit (e.g. letter) is giving
+  //  2. catch arrows actions and switch active answer, then when Z action is pressed -> return this answer, also, 
+  // Possible usage example:
+  // Dialog<string, string> dialog;
+  // DialogProxy proxy;
+  // ...
+  // string data  = d.GetData();
+  // data_wrapped = pm::wrap_into_width( data, box_w, letter_w ); <- also add answers data later
+  // ...
+  // DialogProxy proxy;
+  // proxy.SetDialog( data_wrapped.size(), d.GetAnswers().size() );
+  // proxy.Tick( false, v2() );
+  // data_timed   = data_wrapped.substr( 0, int( data_wrapped.size() * proxy.GetDataFraction()) );
+  // ShowOnScreen( data_timed )
+  
+  // ======= Dialog ====================================================================
+  struct DialogEntry {
+    DialogEntry(){};
+    DialogEntry(string id_, string next_id_, string mark_) : id(id_), next_id(next_id_), mark(mark_) {};
+    string id, next_id, mark;
+  };
   
   template<class A>
-  struct DialogAnswer{
-    string id, next_id, mark;
+  struct DialogAnswer : public DialogEntry {
+    DialogAnswer(){};
+    DialogAnswer(string id_, string next_id_, string mark_) : DialogEntry( id_,  next_id_,  mark_) {};
     A data;
   };
 
   template<class D>
-  struct DialogEntry {
-    string id, next_id, mark;
+  struct DialogSpeech : public DialogEntry {
+    DialogSpeech(){};
+    DialogSpeech(string id_, string next_id_, string mark_) : DialogEntry( id_,  next_id_,  mark_) {};
     D data;
     vector<string> answer_ids;
   };
 
   template<class A, class D>
-  class Dialog {
+  class Dialog : public pm::PmMsg {
     map<string, DialogAnswer<A>> answers;
-    map<string, DialogEntry<D>>  entries;
-    DialogEntry<D> active_entry;
+    map<string, DialogSpeech<D>> speechs;
+    DialogSpeech<D> active_speech;
     
     vector<string> records;
-    void Record(string event){
+    void Record( string event ){
+      PMSG_DEBUG(event);
       records.push_back( event );
     }
     void Record(const DialogAnswer<A> & answer){
-      records.push_back( "answer:" + answer.id );
+      Record( answer_record_mark + answer.id );
       marks += answer.mark;
     }
-    void Record(const DialogEntry<D> & entry){
-      records.push_back( "entry:" + entry.id );
-      marks += entry.mark;
+    void Record(const DialogSpeech<D> & speech){
+      Record( speech_record_mark + speech.id );
+      marks += speech.mark;
     }
   
     public:
-    string state = "none", entry_id, marks;
+    Dialog(){};
     
+    string answer_record_mark = "answer:";
+    string speech_record_mark = "speech:";
+    string state = "none", active_speech_id, marks, def_start_speech_id;
+    
+    void SetStartingSpeech(string id){ def_start_speech_id = id; }
     const string GetState() const { return state; }
-    const vector<string> GetRecord() const { return records; }
-    const string GetMarkRecord() const { return marks; }
+    const vector<string> GetRecords() const { return records; }
+    const string GetMarks() const { return marks; }
+    bool IsStateActive(){ return state == "active"; }
+    bool IsStateEnd(){ return state == "end"; }
+    bool IsStateNone(){ return state == "none"; }
+    bool IsStateReady(){ return state == "ready"; }
     
-    void AddAnswer(string entry_id, string id, A data, string next_id="", string mark=""){
-      DialogAnswer<A> ans = {
-        .id = id,
-        .next_id = next_id,
-        .mark = mark,
-        .data = data
-      };
+    void AddAnswer(string speech_id, string id, A data, string next_id="", string mark=""){
+      auto de = speechs.find( speech_id );
+      if( de == speechs.end() ) {
+        PMSG_WARNING("can't find speech_id = ", speech_id, "to assign answer, return");
+        return;
+      }
+      
+      auto da = answers.find( id );
+      if( da != answers.end() ) {
+        PMSG_WARNING("answer with id = ", id, "already exist, return");
+        return;
+      }
+      
+      de->second.answer_ids.push_back( id );
+      DialogAnswer<A> ans(id, next_id, mark);
+      ans.data = data;
       answers[ id ] = ans;
-      auto de = entries.find( entry_id );
-      if( de != entries.end() ) de->second.answer_ids.push_back( id );
     }
     
-    void AddEntry(string id, D data, string next_id="", string mark=""){
-      if( state == "none" ) state = "ready";
-      DialogEntry<D> ent = {
-        .id = id,
-        .next_id = next_id,
-        .mark = mark,
-        .data = data
-      };
-      entries[ id ] = ent;
+    void AddSpeech(string id, D data, string next_id="", string mark=""){
+      auto de = speechs.find( id );
+      if( de != speechs.end() ) {
+        PMSG_WARNING("speech with id = ", id, "already exist, return");
+        return;
+      }
+    
+      DialogSpeech<D> ent(id, next_id, mark);
+      ent.data = data;
+      speechs[ id ] = ent;
+      
+      /// if it is first speech to add set it as starting entry
+      if( state == "none" ) {
+        state = "ready";
+        SetStartingSpeech(id);
+      }
     }
     
-    D GetData(string id) const { 
-      auto de = entries.find( id );
+    D GetSpeechData(string id) const { 
+      auto de = speechs.find( id );
+      if( de == speechs.end() ) {
+        PMSG_WARNING("can't find speech id = ", id, ", return empty");
+        return D();
+      }
       return de->second.data; 
     };
     
     A GetAnswerData(string id) const { 
       auto de = answers.find( id );
+      if( de == answers.end() ) {
+        PMSG_WARNING("can't find answer id = ", id, ", return empty");
+        return A();
+      }
       return de->second.data; 
     };
     
-    vector<A> GetAnswers(string id) const {
+    vector<A> GetSpeechAnswers(string id) const {
       vector<A> answers;
-      auto de = entries.find( id );
+      auto de = speechs.find( id );
+      if( de == speechs.end() ) {
+        PMSG_WARNING("can't find speech id = ", id, ", return empty");
+        return answers;
+      }
       const vector<string> & answer_ids = de->second.answer_ids;
       for(auto aid : answer_ids){
         answers.push_back( GetAnswerData(aid) );
@@ -132,55 +193,197 @@ namespace ge {
       return answers;
     }
     
-    D GetData() const { return active_entry.data; }
-    vector<A> GetAnswers() const { return GetAnswers( active_entry.id ); }
+    D GetSpeechDataNow() const { return active_speech.data; }
+    vector<A> GetSpeechAnswersNow() const { return GetSpeechAnswers( active_speech.id ); }
     
     void PutAnswer(unsigned int index){
-      if( index < active_entry.answer_ids.size() ) {
-        string id = active_entry.answer_ids[index];
+      if( index < active_speech.answer_ids.size() ) {
+        string id = active_speech.answer_ids[index];
       
-        auto answer = answers[id];
+        DialogAnswer<A> & answer = answers[id];
         Record( answer );
       
-        // check if have next dialog entry
+        // will check if have next dialog speech
         if( MoveTo( answer.next_id ) ) return;
       }
       
-      // check if have default next dialog entry
+      // check if have default next dialog speech
       Record( "answer:default" );
-      if( MoveTo( active_entry.next_id ) ) return;
+      if( MoveTo( active_speech.next_id ) ){
+        return;
+      }
       
       // end of dialog
       End();
     }
     
     bool MoveTo(string id) {
-      auto it = entries.find( id );
-      if( it == entries.end() ) return false;
-      active_entry = it->second;
-      Record( active_entry );
+      PMSG_DEBUG("id", id);
+      auto it = speechs.find( id );
+      if( it == speechs.end() ) return false;
+      active_speech = it->second;
+      Record( active_speech );
       state = "active";
       return true;
     }
     
-    void Start(string entry_id_){
-      entry_id = entry_id_;
+    void Start(string speech_id = ""){
+      if(not speech_id.size() ) active_speech_id = def_start_speech_id;
+      else                      active_speech_id = speech_id;
       Reset();
     }
     
     void Reset(){
       records.clear();
       marks = "";
-      MoveTo( entry_id );
+      if( not active_speech_id.size() ) active_speech_id = def_start_speech_id;
+      MoveTo( active_speech_id );
     }
     
     void End(){
       Record("end");
-      active_entry = DialogEntry<D>();
+      active_speech = DialogSpeech<D>();
+      active_speech_id = "";
       state = "end";
     }
   };
+  
+  // ======= DialogFactory ====================================================================
+  
+  // ======= DialogProxy ====================================================================
+  class DialogProxy {
+    int tick_per_data_unit = 5, freeze_time = 60;
+    int number_of_answers = 0, answer_index = 0;
+    pm::Timer freeze_timer, data_timer;
+    
+    public:
+    DialogProxy(){}
+    DialogProxy( int tick_per_data_unit_, int freeze_time_ ){
+      freeze_time = freeze_time_;
+      tick_per_data_unit = tick_per_data_unit_;
+      freeze_timer = pm::Timer( freeze_time );
+    }
+    
+    bool Tick( bool Z_action, pm::v2 arrows_actions ){
+      bool not_finished = data_timer.TickOneShoot();
+      if( freeze_timer.TickOneShoot() ) return false;
+    
+      // switch active answer
+      if( number_of_answers ){
+        answer_index += pm::check_sign(arrows_actions.y);
+        answer_index = pm::wrap_around(answer_index, 0, number_of_answers);
+      }
 
+      // check Z action
+      if( Z_action ){
+        if( not not_finished ) return true;
+        
+        data_timer.End();
+        freeze_timer.Reset(); 
+      }
+      
+      return false;
+    }
+    
+    int GetAnswer(){
+      return answer_index;
+    }
+    
+    float GetDataFraction(){
+      return data_timer.ftime;
+    }
+    
+    void SetDialog( int number_of_data_units, int number_of_answers_ ){
+      number_of_answers = number_of_answers_;
+      answer_index = 0;
+      data_timer = pm::Timer( tick_per_data_unit * number_of_data_units );
+      freeze_timer.Reset();
+    }
+  };
+  
+  // ======= DialogTypeDefault ====================================================================
+  struct dialog_data {
+    string str, state;
+    map<string, string> meta;
+    string GetText(){ return str; }
+    
+    dialog_data(){};
+    dialog_data( string str_ ){ str = str_; }
+  };
+  
+  class DialogTypeDefault {
+    int box_w;
+    
+    public:
+    string state;
+    Dialog<dialog_data, dialog_data> dialog;
+    DialogProxy proxy;
+    
+    DialogTypeDefault( int box_w_ ){
+      box_w = box_w_;
+    }
+    
+    string WrapSpeechText( string text ){
+      return text + "\n";
+    }
+    
+    string WrapAnswerText( string text, bool active ){
+      if( active ) return " > " + text + "\n";
+      return "   " + text + "\n";
+    }
+    
+    string GetTextFull(){
+      if( not dialog.IsStateActive() ) return "";
+      dialog_data data = dialog.GetSpeechDataNow();
+      string text = WrapSpeechText( data.GetText() );
+      vector<dialog_data> answers = dialog.GetSpeechAnswersNow();
+      int active_answer = proxy.GetAnswer();
+      for( int i = 0; i < answers.size(); i++ ){ 
+        text += WrapAnswerText( answers[i].GetText(), i == active_answer );
+      }
+      return text;
+    }
+    
+    string GetTextWrapped(){
+      string text = GetTextFull();
+      string text_wrapped = pm::wrap_into_width( text, box_w, 1 );
+      string text_timed = text_wrapped.substr( 0, int( text_wrapped.size() * proxy.GetDataFraction()) );
+      return text_timed;
+    }
+    
+    void UpdateData(){
+      if( not dialog.IsStateActive() ){
+        proxy.SetDialog( 0, 0 );
+        return;
+      }
+    
+      string text = GetTextFull();
+      vector<dialog_data> answers = dialog.GetSpeechAnswersNow();
+      proxy.SetDialog( text.size(), answers.size() );
+    }
+    
+    void Reset(){
+      dialog.Reset();
+      UpdateData();
+      state = dialog.state;
+    }
+    
+    void Start(){
+      dialog.Start();
+      UpdateData();
+      state = dialog.state;
+    }
+    
+    void Tick( bool Z_action, pm::v2 arrows_actions ){
+      bool is_answer = proxy.Tick( Z_action, arrows_actions );
+      if( is_answer ){
+        cout << "Tick() = " << Z_action << " " << is_answer << " " << proxy.GetAnswer() << endl;
+        dialog.PutAnswer( proxy.GetAnswer() );
+        UpdateData();
+        state = dialog.state;
+      }
+    }
+  };
 }  
 
 #endif
--- Tests/TestShtaDialogsHLT.cpp
+++ Tests/TestShtaDialogsHLT.cpp
@@ -8,20 +8,22 @@ using namespace ge;
 
 int main(){
 
+  pm::DEFAULT_VERBOSE_LEVEL = pm::verbose::VERBOSE;
+  cout << "=========================== Test 0. Dialog<string, string> constructor & functions" << endl;
   Dialog<string, string> dialog;
   
   //===========================
-  if( dialog.GetState() != "none" ){
+  if( dialog.IsStateNone() != true ){
     cout << "initial state must be \"none\"" << endl;
     return 1;
   }
   cout << "constructor ok" << endl;
   
   //===========================
-  dialog.AddEntry("A", "A", "", "{A}");
-  dialog.AddEntry("B", "B", "", "{B}");
-  dialog.AddEntry("C", "C", "A", "{C}");
-  dialog.AddEntry("D", "D", "", "{D}");
+  dialog.AddSpeech("A", "A", "", "{A}");
+  dialog.AddSpeech("B", "B", "", "{B}");
+  dialog.AddSpeech("C", "C", "A", "{C}");
+  dialog.AddSpeech("D", "D", "", "{D}");
   
   dialog.AddAnswer("A", "A->B", "A->B", "B", "{A->B}");
   dialog.AddAnswer("A", "A->C", "A->C", "C", "{A->C}");
@@ -32,7 +34,7 @@ int main(){
   cout << "setup ok" << endl;
   
   //===========================
-  if( dialog.GetState() != "ready" ){
+  if( dialog.IsStateReady() != true ){
     cout << "next state must be \"ready\"" << endl;
     return 1;
   }
@@ -41,9 +43,9 @@ int main(){
   //===========================
   dialog.Start("A");
   string data = "";
-  data += dialog.GetData();
-  data += dialog.GetAnswers()[0];
-  data += dialog.GetAnswers()[1];
+  data += dialog.GetSpeechDataNow();
+  data += dialog.GetSpeechAnswersNow()[0];
+  data += dialog.GetSpeechAnswersNow()[1];
   
   if( data != "AA->BA->C" ){
     cout << "dialog data incorrect " << data << endl;
@@ -69,10 +71,10 @@ int main(){
   dialog.PutAnswer(0); // move to "end"
 
   //===========================
-  if( dialog.GetMarkRecord() != "{A}{A->B}{B}{B->C}{C}{A}{A->B}{B}{B->D}{D}" ){
-    cout << "marks are incoreect = " << dialog.GetMarkRecord() << endl;
+  if( dialog.GetMarks() != "{A}{A->B}{B}{B->C}{C}{A}{A->B}{B}{B->D}{D}" ){
+    cout << "marks are incoreect = " << dialog.GetMarks() << endl;
     
-    const vector<string> records = dialog.GetRecord();
+    const vector<string> records = dialog.GetRecords();
     cout << "records:" << endl;
     for( auto rec : records ) cout << rec << endl;
   
@@ -88,6 +90,85 @@ int main(){
   cout << "end state ok" << endl;
   
   //===========================
+  cout << "=========================== Test 1. DialogTypeDefault & DialogProxy" << endl;
+  int tick_per_data_unit = 10;
+  int freeze_time = 10;
+  DialogProxy dproxy(tick_per_data_unit, freeze_time);
+  dproxy.SetDialog(5, 5); // 5 speach symbols, 5 answers // 5 * 10 = 50 ticks
+  for(int i = 0; i < 25; i++) dproxy.Tick( false, pm::v2());
+  float fanswer = dproxy.GetDataFraction();
+  if( abs(fanswer - 25./tick_per_data_unit/5.) > 0.000001 ){
+    cout << "DialogProxy.GetDataFraction (1) not correct = " << fanswer << endl;
+    return 1;
+  }
+  cout << "DialogProxy state ok = " << fanswer << endl;
+  
+  dproxy.Tick( true, pm::v2()); // now must be 1.
+  fanswer = dproxy.GetDataFraction();
+  if( abs(fanswer - 1.) > 0.000001 ){
+    cout << "DialogProxy.GetDataFraction (2) not correct = " << fanswer << endl;
+    return 1;
+  }
+  cout << "DialogProxy state ok = " << fanswer << endl;
+  
+  if( dproxy.GetAnswer() != 0 ){    
+    cout << "DialogProxy.GetAnswer() not correct == " << dproxy.GetAnswer() << endl;
+    return 1;
+  }
+  for(int i = 0; i < 25; i++) dproxy.Tick( false, pm::v2());
+  
+  dproxy.Tick( true, pm::v2(0, 1));
+  dproxy.Tick( true, pm::v2(0, 1));
+  if( dproxy.GetAnswer() != 2 ){    
+    cout << "DialogProxy.GetAnswer() not correct == " << dproxy.GetAnswer() << endl;
+    return 1;
+  }
+  
+  Dialog<dialog_data, dialog_data> dialog_dtf;
+  dialog_dtf.AddSpeech("A", dialog_data("some long long long text A"), "", "{A}");
+  dialog_dtf.AddSpeech("B", dialog_data("some long long long text text B"), "", "{B}");
+  dialog_dtf.AddSpeech("C", dialog_data("some long long long text text text C"), "A", "{C}");
+  dialog_dtf.AddSpeech("D", dialog_data("some long long long text text text text D"), "", "{D}");
+  
+  dialog_dtf.AddAnswer("A", "A->B", dialog_data("some long long answer A->B"), "B", "{A->B}");
+  dialog_dtf.AddAnswer("A", "A->C", dialog_data("some long long answer answer answer A->C"), "C", "{A->C}");
+  dialog_dtf.AddAnswer("B", "B->D", dialog_data("some long long answer B->D"), "D", "{B->D}");
+  dialog_dtf.AddAnswer("B", "B->C", dialog_data("some long long answer answer answer B->C"), "C", "{B->C}");
+  
+  int line_width = 20;
+  string def_width = string(line_width, '-');
+  cout << def_width << endl;
+  DialogTypeDefault dtf( line_width );
+  dtf.dialog = dialog_dtf;
+  dtf.proxy = dproxy;
+  dtf.Reset();
+  
+  cout << "constructors ok" << endl;
+  for(int i = 0; i < 35; i++) dtf.Tick( false, pm::v2() );
+  if( dtf.GetTextWrapped() != string("some long long long text A").substr(0, 35/tick_per_data_unit) ){
+    cout << "dtf.GetTextWrapped() not correct = " << dtf.GetTextWrapped() << endl;
+    return 1;
+  }
+  cout << "check 0 ok" << endl;
+  
+  dtf.Tick( true, pm::v2(0, 1) );
+  for(int i = 0; i < freeze_time*2; i++) dtf.Tick( false, pm::v2() );
+  dtf.Tick( true, pm::v2(0, 0) );
+  for(int i = 0; i < 9999; i++) dtf.Tick( false, pm::v2() );
+  dtf.Tick( true, pm::v2() );
+  for(int i = 0; i < 9999; i++) dtf.Tick( false, pm::v2() );
+  dtf.Tick( true, pm::v2() );
+  for(int i = 0; i < 9999; i++) dtf.Tick( false, pm::v2() );
+  dtf.Tick( true, pm::v2() );
+  for(int i = 0; i < 9999; i++) dtf.Tick( false, pm::v2() );
+  dtf.Tick( true, pm::v2() );
+  //dtf.Tick( true, pm::v2() );
+  
+  if( dtf.dialog.GetMarks() != "{A}{A->C}{C}{A}{A->B}{B}{B->D}{D}"){
+    cout << "check 1 not ok, incorrect dialog marks " << dtf.dialog.GetMarks() << endl;
+    return 1;
+  }
+  cout << "check 1 ok" << endl;
   
   return 0;
 }
